96facff8f7d0dca12d77e943c270ab496fb87345
ashisgupta <ashisgupta@ebay.com>
2018-02-25 15:42:34 -0500

    shell sort

---
+++ java/src/test/java/com/brainyvoyage/algos/sort/ShellSortTest.java
@@ -0,0 +1,18 @@
+package com.brainyvoyage.algos.sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShellSortTest {
+
+    private Integer[] data = {10, 80, 30, 90, 40, 50, 70};
+    private Integer[] expected = {10, 30, 40, 50, 70, 80, 90};
+
+    @Test
+    public void sortTest() {
+        ShellSort.sort(data);
+        SortUtils.show(data);
+        assertArrayEquals(expected, data);
+    }
+}
