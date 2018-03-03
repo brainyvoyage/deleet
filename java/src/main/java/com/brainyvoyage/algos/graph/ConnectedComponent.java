90716aef619ff6fd3c253dd3ef895efaa579e4f4
ashisgupta <ashisgupta@ebay.com>
2018-03-03 13:03:15 -0500

    Connected components

---
+++ java/src/main/java/com/brainyvoyage/algos/graph/ConnectedComponent.java
@@ -0,0 +1,42 @@
+package com.brainyvoyage.algos.graph;
+
+public class ConnectedComponent {
+    private boolean[] marked;
+    private int[] id;
+    private int component;
+
+    public ConnectedComponent(Graph graph) {
+        marked = new boolean[graph.getNumVertices()];
+        id = new int[graph.getNumVertices()];
+
+        for (int vertex = 0; vertex < graph.getNumVertices(); vertex++) {
+            if (!marked[vertex]) {
+                marked[vertex] = true;
+                dfs(graph, vertex);
+                component++;
+            }
+        }
+    }
+
+    public boolean connected(int vertex1, int vertex2) {
+        return id[vertex1] == id[vertex2];
+    }
+
+    private void dfs(Graph graph, int source) {
+        marked[source] = true;
+        id[source] = component;
+        for (int connectedVertex : graph.adj(source)) {
+            if (!marked[connectedVertex]) {
+                dfs(graph, connectedVertex);
+            }
+        }
+    }
+
+    public int numConnectedComponent() {
+        return component;
+    }
+
+    public int id(int vertex) {
+        return id[vertex];
+    }
+}
---
+++ java/src/test/java/com/brainyvoyage/algos/graph/ConnectedComponentTest.java
@@ -0,0 +1,44 @@
+package com.brainyvoyage.algos.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConnectedComponentTest {
+    private Graph graph;
+    private ConnectedComponent cc;
+
+    public ConnectedComponentTest() {
+        graph = new Graph(13);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 5);
+        graph.addEdge(0, 6);
+
+        graph.addEdge(3, 5);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 6);
+        graph.addEdge(4, 5);
+
+        graph.addEdge(7, 8);
+
+        graph.addEdge(9, 10);
+        graph.addEdge(9, 11);
+        graph.addEdge(9, 12);
+
+        graph.addEdge(11, 12);
+
+        cc = new ConnectedComponent(graph);
+        System.out.println(graph);
+
+    }
+
+    @Test
+    public void testCC() {
+        assertEquals(3, cc.numConnectedComponent());
+        assert (cc.connected(0, 4));
+        assert (!cc.connected(2, 12));
+        assert (!cc.connected(7, 10));
+    }
+
+}
