9bf3fbafac924bfe7ca6d3cead28da0aeb8c9fb0
ashisgupta <ashisgupta@ebay.com>
2018-03-03 11:59:46 -0500

    BFS

---
+++ java/src/main/java/com/brainyvoyage/algos/graph/BreadthFirstPaths.java
@@ -0,0 +1,56 @@
+package com.brainyvoyage.algos.graph;
+
+import com.brainyvoyage.datastructure.Queue;
+
+import java.util.Stack;
+
+public class BreadthFirstPaths {
+
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final Integer source;
+
+    /**
+     * @param graph
+     * @param source
+     */
+    public BreadthFirstPaths(Graph graph, int source) {
+        marked = new boolean[graph.getNumVertices()];
+        edgeTo = new int[graph.getNumVertices()];
+        this.source = source;
+        bfs(graph, source);
+    }
+
+    private void bfs(Graph graph, int source) {
+        marked[source] = true;
+        Queue<Integer> visitingQueue = new Queue<>();
+        visitingQueue.enqueue(source);
+        while (!visitingQueue.isEmpty()) {
+            Integer vertex = visitingQueue.dequeue();
+            for (int connectedVertices : graph.adj(vertex)) {
+                if (!marked[connectedVertices]) {
+                    visitingQueue.enqueue(connectedVertices);
+                    edgeTo[connectedVertices] = vertex;
+                    marked[connectedVertices] = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param vertex
+     * @return
+     */
+    public boolean hasPathTo(int vertex) {
+        return marked[vertex];
+    }
+
+    public Iterable<Integer> pathTo(int vertex) {
+        if (!marked[vertex]) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int x = vertex; x != source; x = edgeTo[x]) {
+            path.push(x);
+        }
+        return path;
+    }
+}
---
+++ java/src/test/java/com/brainyvoyage/algos/graph/BreadthFirstPathsTest.java
@@ -0,0 +1,50 @@
+package com.brainyvoyage.algos.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BreadthFirstPathsTest {
+    private final Graph graph;
+    private final BreadthFirstPaths bfsPath;
+    private String pathString(Iterable<Integer> path, Integer source) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer vertex : path) {
+            if (vertex == 0) break;
+            sb.append(vertex + "-");
+        }
+        sb.append(source);
+        return sb.reverse().toString();
+    }
+    public BreadthFirstPathsTest() {
+        graph = new Graph(7);
+        graph.addEdge(0, 5);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 1);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(0, 2);
+
+        bfsPath = new BreadthFirstPaths(graph, 0);
+        System.out.print(graph);
+    }
+
+    @Test
+    public void testPath() {
+        assert (bfsPath.hasPathTo(3));
+        assert (!bfsPath.hasPathTo(6));
+    }
+
+    @Test
+    public void testPathTo() {
+        Iterable<Integer> path = bfsPath.pathTo(5);
+        assertNotEquals(null, path);
+
+        System.out.println(pathString(path, 0));
+        path = bfsPath.pathTo(6);
+        assertEquals(null, path);
+    }
+}
