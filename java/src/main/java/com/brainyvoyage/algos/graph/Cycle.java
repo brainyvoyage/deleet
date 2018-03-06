d1e311b68552c6093a3399c052e632cbc7ccfb58
ashisgupta <ashisgupta@ebay.com>
2018-03-06 08:30:11 -0500

    Detect cycle in graph

---
+++ java/src/main/java/com/brainyvoyage/algos/graph/Cycle.java
@@ -0,0 +1,29 @@
+package com.brainyvoyage.algos.graph;
+
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph graph) {
+        marked = new boolean[graph.getNumVertices()];
+        for (int vertex = 0; vertex < graph.getNumVertices(); vertex++) {
+            if (!marked[vertex]) {
+                marked[vertex] = true;
+                dfs(graph, vertex, vertex);
+            }
+        }
+    }
+
+    private void dfs(Graph graph, int vertex1, int vertex2) {
+        marked[vertex1] = true;
+        for (int connectedVertex : graph.adj(vertex1)) {
+            if (!marked[connectedVertex]) {
+                dfs(graph, connectedVertex, vertex2);
+            } else if (vertex1 != vertex2) hasCycle = true;
+        }
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+}
---
+++ java/src/test/java/com/brainyvoyage/algos/graph/CycleTest.java
@@ -0,0 +1,37 @@
+package com.brainyvoyage.algos.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CycleTest {
+    private Graph graph;
+    private Cycle cycle;
+    public CycleTest() {
+        graph = new Graph(13);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 5);
+        graph.addEdge(0, 6);
+
+        graph.addEdge(3, 5);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 6);
+        graph.addEdge(4, 5);
+
+        graph.addEdge(7, 8);
+
+        graph.addEdge(9, 10);
+        graph.addEdge(9, 11);
+        graph.addEdge(9, 12);
+
+        graph.addEdge(11, 12);
+        cycle = new Cycle(graph);
+    }
+
+    @Test
+    public void hasCycle() {
+        System.out.print(cycle.hasCycle());
+        assert (cycle.hasCycle());
+    }
+}
